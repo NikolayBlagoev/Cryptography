62aec725d7c253271b4228af1794169ec9f5236d
NikolayBlagoev <62670626+NikolayBlagoev@users.noreply.github.com>
2022-06-16 00:26:25 +0300

    Updated the ceasar cipher

--- ceasar/ceasar_cipher.js
+++ ceasar/ceasar_cipher.js
@@ -13,6 +13,7 @@ function onClickEncr(){
     var keys = getKeys()
     if(keys.length!=2){
         out.value = "INVALID KEYS! A NEEDS TO BE MAPPED TO 1 CHARACTER AND INCREMENT NEEDS TO BE AN INTEGER"
+        return;
     }
     console.log(keys)
     out.value = encrypt(a, keys[0],keys[1])
@@ -27,6 +28,7 @@ function onClickDecr(){
     var keys = getKeys()
     if(keys.length!=2){
         out.value = "INVALID KEYS! A NEEDS TO BE MAPPED TO 1 CHARACTER AND INCREMENT NEEDS TO BE AN INTEGER"
+        return;
     }
     out.value = decrypt(a, keys[0],keys[1])
     
@@ -42,7 +44,7 @@ function getKeys(){
     keys.push(k1.charCodeAt(0)-a)
     var k2 = document.getElementById("key2").value;
     if(isNaN(k2)||k2<0) return keys;
-    keys.push(k2)
+    keys.push(parseInt(k2,10))
     return keys
 }
 function encrypt(in_string, diff, increment){
@@ -52,11 +54,12 @@ function encrypt(in_string, diff, increment){
         c=in_string.charCodeAt(i)
         console.log(in_string.charAt(i))
         if(c>=a && c<=z){
-            console.log(String.fromCharCode(((c-a+diff+added)%modulo)+a))
+            console.log(((c-a+diff+added)%modulo)+a)
             out+=String.fromCharCode(((c-a+diff+added)%modulo)+a)
             added = (added+increment)%modulo
+            console.log(added)
         }else if(c>=A && c<=Z){
-            console.log(String.fromCharCode(((c-A+diff+added)%modulo)+A))
+            console.log(((c-A+diff+added)%modulo)+A)
             out+= String.fromCharCode(((c-A+diff+added)%modulo)+A)
             added = (added+increment)%modulo
         }else{
